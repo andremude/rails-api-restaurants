e629de8a34bac36d70a4c32a32783df4f90e69ff
Andres Munoz <andremude@hotmail.com>
2022-08-05 20:16:22 -0500

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_05_031159) do
+ActiveRecord::Schema.define(version: 2022_08_06_005705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2022_08_05_031159) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2022_08_05_031159) do
   end
 
   add_foreign_key "comments", "restaurants"
+  add_foreign_key "restaurants", "users"
 end
