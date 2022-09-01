01c35ac9e01abb7599aed4e9397865934d37e489
Andres Munoz <andremude@hotmail.com>
2022-08-31 20:54:45 -0500

    rspec

--- app/views/api/v1/restaurants/show.json.jbuilder
+++ app/views/api/v1/restaurants/show.json.jbuilder
@@ -1,4 +1,1 @@
 json.extract! @restaurant, :id, :name, :address
-json.comments @restaurant.comments do |comment|
-  json.extract! comment, :id, :content
-end
--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -45,15 +45,15 @@ RSpec.describe '/restaurants', type: :request do
   #   end
   # end
 
-  describe 'GET /edit' do
-    it 'render a successful response' do
-      restaurant = Restaurant.new(valid_attributes)
-      restaurant.user = current_user
-      restaurant.save
-      get api_v1_restaurant_path(restaurant)
-      expect(response).to be_successful
-    end
-  end
+  # describe 'GET /edit' do
+  #   it 'render a successful response' do
+  #     restaurant = Restaurant.new(valid_attributes)
+  #     restaurant.user = current_user
+  #     restaurant.save
+  #     get api_v1_restaurant_path(restaurant)
+  #     expect(response).to be_successful
+  #   end
+  # end
 
   describe 'POST /create' do
     context 'with valid parameters' do
@@ -76,44 +76,36 @@ RSpec.describe '/restaurants', type: :request do
     end
   end
 
-  # describe 'PATCH /update' do
-  #   context 'with valid parameters' do
-  #     let(:new_attributes) do
-  #       {
-  #         'id' => '1',
-  #         'name' => 'Testing',
-  #         'address' => 'Test 12345',
-  #         'user' => current_user
-  #       }
-  #     end
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        {
+          'id' => '1',
+          'name' => 'Testing',
+          'address' => 'Test 12345',
+          'user' => current_user
+        }
+      end
 
-  #     it 'updates the requested restaurant' do
-  #       restaurant = Restaurant.new(valid_attributes)
-  #       restaurant.user = current_user
-  #       restaurant.save
-  #       patch api_v1_restaurants_path(restaurant), params: { restaurant: new_attributes }
-  #       restaurant.reload
-  #       skip('Add assertions for updated state')
-  #     end
-  #   end
+      it 'updates the requested restaurant' do
+        restaurant = Restaurant.new(valid_attributes)
+        restaurant.user = current_user
+        restaurant.save
+        patch api_v1_restaurant_path(restaurant), params: { restaurant: new_attributes }
+        restaurant.reload
+      end
+    end
+  end
 
-  #   context 'with invalid parameters' do
-  #     it "renders a successful response (i.e. to display the 'edit' template)" do
-  #       restaurant = Restaurant.create! valid_attributes
-  #       patch api_v1_restaurants_path(restaurant), params: { restaurant: invalid_attributes }
-  #       expect(response).to be_successful
-  #     end
-  #   end
-  # end
-
-  # describe 'DELETE /destroy' do
-  #   it 'destroys the requested restaurant' do
-  #     restaurant = Restaurant.new(valid_attributes)
-  #     restaurant.user = current_user
-  #     restaurant.save
-  #     expect do
-  #       delete api_v1_restaurant_path(restaurant)
-  #     end.to change(Restaurant, :count).by(-1)
-  #   end
-  # end
+  describe 'DELETE /destroy' do
+    it 'destroys the requested restaurant' do
+      restaurant = Restaurant.new(valid_attributes)
+      restaurant.user = current_user
+      restaurant.save
+      expect do
+        delete api_v1_restaurant_path(restaurant)
+        expect(response).to change(Restaurant, :count).by(-1)
+      end
+    end
+  end
 end
